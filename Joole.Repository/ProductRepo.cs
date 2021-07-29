64780746d8e5e6d962dc7b926aa76ceb7af99d77
f252143511 <45050122+f252143511@users.noreply.github.com>
2021-07-29 12:08:41 -0700

    June 29

---
+++ Joole.Repository/ProductRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Joole.Dal;
+
+namespace Joole.Repository
+{
+
+    public interface IProductRepo : IRepository<Product>
+    {
+
+    }
+    public class ProductRepo : Repository<Product>, IProductRepo
+    {
+        public ProductRepo(DbContext context) : base(context)
+        {
+
+        }
+    }
+}
