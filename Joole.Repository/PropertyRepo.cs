5da19a093660c02695c931ade80877fba8819726
ioavliv <ilivneh@nyit.edu>
2021-07-30 16:10:55 -0400

    added property repos

---
+++ Joole.Repository/PropertyRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Joole.Dal;
+
+namespace Joole.Repository
+{
+
+    public interface IPropertyRepo : IRepository<Property>
+    {
+
+    }
+    public class PropertyRepo : Repository<Property>, IPropertyRepo
+    {
+        public PropertyRepo(DbContext context) : base(context)
+        {
+
+        }
+    }
+}
